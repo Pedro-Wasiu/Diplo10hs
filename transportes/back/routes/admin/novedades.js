b1f85d005ea4cdf5d28be8fb07c0b764a03825b8
¨Pedro <pedrowasiu@gmail.com>
2025-01-31 10:28:26 -0300

    M6U1

--- transportes/back/routes/admin/novedades.js
+++ transportes/back/routes/admin/novedades.js
@@ -1,15 +1,46 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
-// Middleware `secured` importado desde app.js
-const { secured } = require('../../app'); // Asegúrate de que esta ruta sea correcta
+// para listar las novedades
+router.get('/', async function (req, res, next) {
+    var novedades = await novedadesModel.getNovedades();
 
-// Ruta protegida para `novedades`
-router.get('/', secured, function (req, res, next) {
     res.render('admin/novedades', {
         layout: 'admin/layout',
-        usuario: req.session.nombre, // Accediendo al nombre del usuario desde la sesión
+        usuario: req.session.nombre,
+        novedades
     });
 });
+
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', { // agregar.hbs
+        layout: 'admin/layout'
+    });
+}); // cierra Get
+
+router.post('/agregar', async (req, res, next) => {
+    try {
+        if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+            await novedadesModel.insertNovedad(req.body);
+            res.redirect('/admin/novedades');
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true,
+                message: 'Todos los campos son requeridos'
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se cargó la novedad'
+        });
+    }
+});
+
+
 
 module.exports = router;
